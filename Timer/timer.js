bb24ac1b3ce333bd80fdd4aa2f0fe94cffeee51b
Anshul-Ray <rayanshul19@gmail.com>
2021-06-08 16:41:03 +0530

    Updates

--- Timer/timer.js
+++ Timer/timer.js
@@ -2,6 +2,9 @@ const timer = document.getElementById("sw");
 let tbar = 0;
 let sbar = 0;
 let mux = 0;
+const c1 = "green";
+const c2 = "yellow";
+const c3 = "red";
 var xh = 0;
 var xm = 0;
 var xs = 0;
@@ -25,6 +28,10 @@ function init()
     xmin = localStorage.getItem('xminutes');
     xsec = localStorage.getItem('xseconds');
     tbar = parseInt(xhrs*60*60)+parseInt(xmin*60)+parseInt(xsec);
+    mux = 100/tbar;
+}
+function reinit()
+{
     if((xsec == 0 || xsec < 1) && xmin > 0)
     {
         xmin--;
@@ -39,6 +46,7 @@ function init()
 }
 init();
 TimeDisp(xhrs,xmin,xsec);
+reinit();
 function startTimer()
 {
     if(xhrs>0 || xmin>0 || xsec>0)
@@ -71,18 +79,28 @@ function timerCyc()
         }
         sbar++;
         var elem = document.getElementById("clkBar");
-        var id = setInterval(frame,1);
+        var id = setInterval(frame,100);
         function frame()
         {
+            var per;
+            per = parseFloat(sbar/tbar)*100;
+            console.log(per);
             if (sbar > tbar )
             {clearInterval(id);}
             else
             {
+                if(per<=50)
+                {elem.style.backgroundColor = c1;}
+                else if(per > 50 && per < 75)
+                {elem.style.backgroundColor = c2;}
+                else
+                {elem.style.backgroundColor = c3;}
                 elem.style.width = sbar*mux + "%";
             }
         }
     },1000);
 }
+
 function endTimeDisp()
 {
     clearInterval(cyc);
@@ -109,8 +127,7 @@ function resetTimer()
 {
     clearInterval(cyc);
     sbar = 0;
-    xhrs = localStorage.getItem('xhours');
-    xmin = localStorage.getItem('xminutes');
-    xsec = localStorage.getItem('xseconds');    
+    init();    
     TimeDisp(xhrs,xmin,xsec);
+    reinit();
 }
