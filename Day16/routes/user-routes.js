020505622fbd8a710308f858422302721ba2b210
Anshul-Ray <84887942+Anshul-Ray@users.noreply.github.com>
2021-06-12 23:20:26 +0530

    Updates

---
+++ Day16/routes/user-routes.js
@@ -0,0 +1,17 @@
+// Other libraries importing
+const express = require('express');
+
+// Self created functions/files/classes
+const userController = require('../controller/user-controller');
+
+
+const router = express.Router();
+
+
+
+router.post("/signup",  userController.userSignup);
+router.post("/login", userController.userLogin);
+router.get("/getInfo/:userName/:userAge", userController.userInfo);
+
+
+module.exports = router;
